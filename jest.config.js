84ea935e386a5d6527cafdd842f4fabcd11e7653
Neto test <maleeva51@gmail.com>
2025-12-24 12:36:31 +0300

    game

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js'
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-};
