a4100b290d53d52df264af6a67f44e470154ba11
Neto test <maleeva51@gmail.com>
2026-01-02 11:37:49 +0300

    obnovlenie

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './style.css';
+import goblinImage from './assets/goblin.png';
+import { Game } from './game';
 
+// Инициализация игры
+const game = new Game(goblinImage);
+game.init();
+
+// Экспортируем для тестов
+export { game };
